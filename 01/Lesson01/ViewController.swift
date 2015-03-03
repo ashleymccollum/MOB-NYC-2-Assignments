4c5b1691c8f2850972d04987748bee84a635db0a
Ashley McCollum <ashleymccollum@Ashleys-MacBook-Pro.local>
2015-03-03 04:26:07 -0500

    test 2 images

--- 01/Lesson01/ViewController.swift
+++ 01/Lesson01/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func buttonClicked
 
     override func viewDidLoad() {
         super.viewDidLoad()
